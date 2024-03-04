b8f2d066e8a4c88f9d230cbf5323fd1ca34480b2
THEMADMAXPRO <157195522+THEMADMAXPRO@users.noreply.github.com>
2024-03-04 16:49:25 +0530

    Update thumbnails.py

--- MadmaxXMusic/utils/thumbnails.py
+++ MadmaxXMusic/utils/thumbnails.py
@@ -1,13 +1,9 @@
 import os
 import re
-import random
 
 import aiofiles
 import aiohttp
-
-from PIL import Image, ImageDraw, ImageEnhance
-from PIL import ImageFilter, ImageFont, ImageOps
-
+from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
@@ -68,63 +64,52 @@ async def get_thumb(videoid):
                     await f.write(await resp.read())
                     await f.close()
 
-        
-        colors = ["white", "red", "orange", "yellow", "green", "cyan", "azure", "blue", "violet", "magenta", "pink"]
-        border = random.choice(colors)
         youtube = Image.open(f"cache/thumb{videoid}.png")
         image1 = changeImageSize(1280, 720, youtube)
-        bg_bright = ImageEnhance.Brightness(image1)
-        bg_logo = bg_bright.enhance(1.1)
-        bg_contra = ImageEnhance.Contrast(bg_logo)
-        bg_logo = bg_contra.enhance(1.1)
-        logox = ImageOps.expand(bg_logo, border=7, fill=f"{border}")
-        background = changeImageSize(1280, 720, logox)
-        # image2 = image1.convert("RGBA")
-        # background = image2.filter(filter=ImageFilter.BoxBlur(1))
-        #enhancer = ImageEnhance.Brightness(background)
-        #background = enhancer.enhance(0.9)
-        #draw = ImageDraw.Draw(background)
-        #arial = ImageFont.truetype("MadmaxXMusic/assets/font2.ttf", 30)
-        #font = ImageFont.truetype("MadmaxXMusic/assets/font.ttf", 30)
-        # draw.text((1110, 8), unidecode(app.name), fill="white", font=arial)
-        """
+        image2 = image1.convert("RGBA")
+        background = image2.filter(filter=ImageFilter.BoxBlur(10))
+        enhancer = ImageEnhance.Brightness(background)
+        background = enhancer.enhance(0.5)
+        draw = ImageDraw.Draw(background)
+        arial = ImageFont.truetype("MadmaxXMusic/assets/font2.ttf", 30)
+        font = ImageFont.truetype("MadmaxXMusic/assets/font.ttf", 30)
+        draw.text((1110, 8), unidecode(app.name), fill="white", font=arial)
         draw.text(
-            (1, 1),
+            (55, 560),
             f"{channel} | {views[:23]}",
-            (1, 1, 1),
+            (255, 255, 255),
             font=arial,
         )
         draw.text(
-            (1, 1),
+            (57, 600),
             clear(title),
-            (1, 1, 1),
+            (255, 255, 255),
             font=font,
         )
         draw.line(
-            [(1, 1), (1, 1)],
+            [(55, 660), (1220, 660)],
             fill="white",
-            width=1,
+            width=5,
             joint="curve",
         )
         draw.ellipse(
-            [(1, 1), (2, 1)],
+            [(918, 648), (942, 672)],
             outline="white",
             fill="white",
-            width=1,
+            width=15,
         )
         draw.text(
-            (1, 1),
+            (36, 685),
             "00:00",
-            (1, 1, 1),
+            (255, 255, 255),
             font=arial,
         )
         draw.text(
-            (1, 1),
+            (1185, 685),
             f"{duration[:23]}",
-            (1, 1, 1),
+            (255, 255, 255),
             font=arial,
         )
-        """
         try:
             os.remove(f"cache/thumb{videoid}.png")
         except:
