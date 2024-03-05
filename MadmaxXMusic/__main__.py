deb647d82b12b06292c34b6d3cb2dc18d5013f72
THEMADMAXPRO <157195522+THEMADMAXPRO@users.noreply.github.com>
2024-03-05 12:06:41 +0530

    Update __main__.py

--- MadmaxXMusic/__main__.py
+++ MadmaxXMusic/__main__.py
@@ -40,7 +40,7 @@ async def init():
     await userbot.start()
     await Madmax.start()
     try:
-        await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await Madmax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("MadmaxXMusic").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
