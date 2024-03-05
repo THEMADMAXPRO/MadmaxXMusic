81a387f4bc553e2553f92beaf1d6ba62ec240c4f
THEMADMAXPRO <157195522+THEMADMAXPRO@users.noreply.github.com>
2024-03-05 07:27:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/THEMADMAXPRO/MadmaxXMusic",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "MadmaxXMusic")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "madmax")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
