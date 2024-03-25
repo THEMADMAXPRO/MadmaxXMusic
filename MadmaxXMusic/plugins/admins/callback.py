a512158036827bac17c7006804a33312b5beebe9
THEMADMAXPRO <157195522+THEMADMAXPRO@users.noreply.github.com>
2024-03-25 20:14:11 +0530

    Update callback.py

--- MadmaxXMusic/plugins/admins/callback.py
+++ MadmaxXMusic/plugins/admins/callback.py
@@ -135,7 +135,7 @@ async def del_back_playlist(client, CallbackQuery, _):
             return await CallbackQuery.answer(_["admin_1"], show_alert=True)
         await CallbackQuery.answer()
         await music_off(chat_id)
-        await Anony.pause_stream(chat_id)
+        await Madmax.pause_stream(chat_id)
         await CallbackQuery.message.reply_text(
             _["admin_2"].format(mention), reply_markup=close_markup(_)
         )
@@ -144,13 +144,13 @@ async def del_back_playlist(client, CallbackQuery, _):
             return await CallbackQuery.answer(_["admin_3"], show_alert=True)
         await CallbackQuery.answer()
         await music_on(chat_id)
-        await Anony.resume_stream(chat_id)
+        await Madmax.resume_stream(chat_id)
         await CallbackQuery.message.reply_text(
             _["admin_4"].format(mention), reply_markup=close_markup(_)
         )
     elif command == "Stop" or command == "End":
         await CallbackQuery.answer()
-        await Anony.stop_stream(chat_id)
+        await Madmax.stop_stream(chat_id)
         await set_loop(chat_id, 0)
         await CallbackQuery.message.reply_text(
             _["admin_5"].format(mention), reply_markup=close_markup(_)
