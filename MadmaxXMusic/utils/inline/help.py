5ab506c865cb6ee896544312c9cd275f204713a3
THEMADMAXPRO <157195522+THEMADMAXPRO@users.noreply.github.com>
2024-03-25 06:42:48 +0530

    Update help.py

--- MadmaxXMusic/utils/inline/help.py
+++ MadmaxXMusic/utils/inline/help.py
@@ -28,7 +28,7 @@ def first_page(_):
 # creating second partition of menu
 
 def second_page(_):
-	controll_button = [InlineKeyboardButton(text="◁", callback_data=f"settings_back_helper_fixed"), InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settings_back_helper_fixed"), InlineKeyboardButton(text="▷", callback_data=f"settings_back_helper")]
+	controll_button = [InlineKeyboardButton(text="◁", callback_data=f"settings_back_helper_fixed"), InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper"), InlineKeyboardButton(text="▷", callback_data=f"settings_back_helper")]
 	second_page_menu = InlineKeyboardMarkup(
 		[
 			[InlineKeyboardButton(text=_["H_B_13"], callback_data="help_callback hb13"), InlineKeyboardButton(text=_["H_B_14"], callback_data="help_callback hb14"), InlineKeyboardButton(text=_["H_B_15"], callback_data="help_callback hb15")],
