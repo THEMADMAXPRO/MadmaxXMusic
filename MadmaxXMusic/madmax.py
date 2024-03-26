42489a99ce2cb37a1f3f955a9be12e9d3bf67193
THEMADMAXPRO <157195522+THEMADMAXPRO@users.noreply.github.com>
2024-03-26 18:40:05 +0530

    Create madmax.py

---
+++ MadmaxXMusic/madmax.py
@@ -0,0 +1,14 @@
+EMMA_PICS = [
+"https://telegra.ph/file/a5bab2979b394358a85fb.jpg",
+"https://telegra.ph/file/c4d55ee5bf4b897b7c889.jpg",
+"https://telegra.ph/file/5cbd0785e1ecc88e76b64.jpg",
+"https://telegra.ph/file/891742a91fc82e3e500d1.jpg",
+"https://telegra.ph/file/c5211b79c5227cde1dbea.jpg",
+"https://telegra.ph/file/ac288f21162efea13e359.jpg",
+"https://telegra.ph/file/716647c16254aa77fd189.jpg",
+"https://telegra.ph/file/b8c11b32abef6fce3081f.jpg",
+"https://telegra.ph/file/93fd70f204ed30e32f220.jpg",
+"https://telegra.ph/file/19fa60571da4e42361ce9.jpg",
+"https://telegra.ph/file/02dd4f129ba4d7a3f2813.jpg",
+"https://telegra.ph/file/57e109386e8a6075eff52.jpg"
+]
