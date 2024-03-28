293f15fad240d686a0fbaa1fb326cef92b91dafe
THEMADMAXPRO <157195522+THEMADMAXPRO@users.noreply.github.com>
2024-03-28 18:54:44 +0530

    Update extras.py

--- MadmaxXMusic/utils/inline/extras.py
+++ MadmaxXMusic/utils/inline/extras.py
@@ -1,4 +1,4 @@
-/eval from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import SUPPORT_CHAT
 
