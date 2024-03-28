8e9dbbbaaa67670748a32d0dd94d56ed4e2b9e13
THEMADMAXPRO <157195522+THEMADMAXPRO@users.noreply.github.com>
2024-03-28 18:52:20 +0530

    Update extras.py

--- MadmaxXMusic/utils/inline/extras.py
+++ MadmaxXMusic/utils/inline/extras.py
@@ -1,4 +1,4 @@
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+/eval from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import SUPPORT_CHAT
 
@@ -8,7 +8,7 @@ def botplaylist_markup(_):
         [
             InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
-            InlineKeyboardButton(text="ᴧᴅᴅ ᴛʜɪs ʙσᴛ ɪɴ ʏσᴜʀ ɢʀσᴜᴘ", url="https://t.me/emma_musicop_bot?startgroup=true"),
+            InlineKeyboardButton(text="ᴧᴅᴅ ᴛʜɪs ʙσᴛ ɪɴ ʏσᴜʀ ɢʀσᴜᴘ", url="https://t.me/{app.username}?startgroup=true"),
         ],
     ]
     return buttons
@@ -45,9 +45,8 @@ def add_markup(_):
     upl = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(text="ᴧᴅᴅ ᴛʜɪs ʙσᴛ ɪɴ ʏσᴜʀ ɢʀσᴜᴘ", url="https://t.me/emma_musicop_bot?startgroup=true"),
+                InlineKeyboardButton(text="ᴧᴅᴅ ᴛʜɪs ʙσᴛ ɪɴ ʏσᴜʀ ɢʀσᴜᴘ", url="https://t.me/{app.username}?startgroup=true"),
            ],
         ]
     )
     return upl
-
